caf0f2e5dda707fb577538c20dd1d8d081801a69
minimillie <60392831+minimillie@users.noreply.github.com>
2020-02-13 14:08:35 -0500

    Update main.js

--- DigitalAssignment1/js/main.js
+++ DigitalAssignment1/js/main.js
@@ -15,6 +15,7 @@ window.onload = function() {
     function preload() {
         
         game.load.image( 'bird', 'assets/bird.png' );
+        game.load.image( 'pipe', 'assers/pipe.png' );
     }
     
     var bouncy;
@@ -34,6 +35,10 @@ window.onload = function() {
         var spaceKey = game.input.keyboard.addKey(
                         Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);
+        
+        this.pipes = game.add.group();
+        
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
     }
     
     function update() {
@@ -47,5 +52,27 @@ window.onload = function() {
     
     function restartGame() {
         game.state.start('main');
+    }
+    
+    function addOnePipe(x,y) {
+       var pipe = game.add.sprite(x, y, 'pipe');
+        
+       this.pipes.add(pipe);
+        
+       game.physics.arcade.enable(pipe);
+        
+       pipe.body.velocity.x = -200;
+        
+       pipe.checkWorldBounds = true;
+       pipe.outOfBoundsKill = true;
+    }
+    
+    function addRowOfPipes() {
+        
+       var hole = Math.floor(Math.random() * 5) + 1;
+        
+       for (var i = 0; i < 8; i++)
+           if (i != hole && i != hole + 1)
+               this.addOnePipe(400, i * 60 + 10);
     }
 };
