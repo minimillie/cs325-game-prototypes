2a84abef8442de38b6515e93d13c6cf3c7252b88
minimillie <60392831+minimillie@users.noreply.github.com>
2020-02-13 08:58:07 -0500

    Update main.js

--- DigitalAssignment1/js/main.js
+++ DigitalAssignment1/js/main.js
@@ -1,12 +1,28 @@
-// Create our 'main' state that will contain the game
-var mainState = {
-    preload: function() { 
-        game.load.image('bird','assests/bone.png');
-    },
+"use strict";
 
-    create: function() { 
-        game.stage.backgroundColor = '#71c5cf';
+window.onload = function() {
+    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
+    // You will need to change the fourth parameter to "new Phaser.Game()" from
+    // 'phaser-example' to 'game', which is the id of the HTML element where we
+    // want the game to go.
+    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
+    // You will need to change the paths you pass to "game.load.image()" or any other
+    // loading functions to reflect where you are putting the assets.
+    // All loading functions will typically all be found inside "preload()".
+    
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    
+    function preload() {
+        // Load an image and call it 'logo'.
+        game.load.image( 'bird', 'assets/bone.png' );
+    }
+    
+    var bouncy;
+    
+    function create() {
         
+        game.stage.backgroundColor = '#71c5cf';
+       
         game.physics.startSystem(Phaser.Physics.ARCADE);
         
         this.bird = game.add.sprite(100, 245, 'bird');
@@ -15,34 +31,22 @@ var mainState = {
         
         this.bird.body.gravity.y = 1000;
         
-        var spaceKey = game.input.keyboard.addKey (
+        var spaceKey = game.input.keybaord.addKey)
                         Phaser.Keyboard.SPACEBAR);
-        
         spaceKey.onDown.add(this.jump, this);
-        
-    },
-
-    update: function() {
-        // This function is called 60 times per second    
-        // It contains the game's logic   
-        if (this.bird.y < 0 || this.bird.y > 490)
-            this.restartGame();
-    },
-};
-
-jump: function() {
-    this.bird.body.velocity.y = -350;
-},
+    }
     
-restartGame: function() {
-    game.state.start('main');
-},
-
-// Initialize Phaser, and create a 400px by 490px game
-var game = new Phaser.Game(400, 490);
-
-// Add the 'mainState' and call it 'main'
-game.state.add('main', mainState); 
-
-// Start the state to actually start the game
-game.state.start('main');
+    function update() {
+       if (this.bird.y < 0 || this.bird.y > 490)
+           this.restartGame()
+    }
+    
+    jump: function() {
+        this.bird.body.velocity.y = -350;
+    }
+    
+    restartGame: function() {
+        game.state.start('main');
+    }
+};
+© 2020 GitHub, Inc.
