c58ee023e748d97a60b37088aa56e10c020f4f99
minimillie <60392831+minimillie@users.noreply.github.com>
2020-02-14 14:17:13 -0500

    Update main.js

--- DigitalAssignment1/js/main.js
+++ DigitalAssignment1/js/main.js
@@ -39,11 +39,19 @@ window.onload = function() {
         this.pipes = game.add.group();
         
         this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+        
+        this.score = 0;
+        
+        this.labelScore = game.add.text(20, 20, "0",
+                                        { font: "30px Arial", fill: "#ffffff" });
+        
     }
     
     function update() {
        if (this.bird.y < 0 || this.bird.y > 490)
            this.restartGame()
+       game.physics.arcade.overlap (
+           this.bird, this.pipes, this.restartGame, null, this);
     }
     
     function jump() {
@@ -74,5 +82,7 @@ window.onload = function() {
        for (var i = 0; i < 8; i++)
            if (i != hole && i != hole + 1)
                this.addOnePipe(400, i * 60 + 10);
+               this.score += 1;
+               this.labelScore.text = this.score;
     }
 };
