bda6a02e5d145eba78cfd651bf15ea6acab0ba89
minimillie <60392831+minimillie@users.noreply.github.com>
2020-02-14 13:51:03 -0500

    Update main.js

--- DigitalAssignment1/js/main.js
+++ DigitalAssignment1/js/main.js
@@ -10,7 +10,7 @@ window.onload = function() {
     // loading functions to reflect where you are putting the assets.
     // All loading functions will typically all be found inside "preload()".
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, jump: jump, restartGame: restartGame } );
+    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update, jump: jump, restartGame: restartGame, addOnePipe: addOnePipe, addRowOfPipes: addRowOfPipes } );
     
     function preload() {
         
