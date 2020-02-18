143924e5d42c93dbff5f9d2c14dd513c72d4fb01
minimillie <60392831+minimillie@users.noreply.github.com>
2020-02-18 13:01:22 -0500

    Update main.js

--- game2/js/main.js
+++ game2/js/main.js
@@ -1,49 +1,165 @@
 "use strict";
 
 window.onload = function() {
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
-    
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+  var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, updateBullets: updateBullets, fireBullet: fireBullet });
+
+function preload () {
+
+    game.load.image('player', 'assets/ghost.png');
+    game.load.image('star', 'assets/bourne.png');
+    game.load.image('baddie', 'assets/reds.png');
+    game.load.atlas('lazer', 'assets/_MAP.png', 'assets/_MAP.json');
+
+}
+
+var stars;
+var baddies;
+var lazers;
+var player;
+var cursors;
+var fireButton;
+var bulletTime = 0;
+var frameTime = 0;
+var frames;
+var prevCamX = 0;
+
+function create () {
+
+    game.world.setBounds(0, 0, 800*4, 600);
+
+    frames = Phaser.Animation.generateFrameNames('frame', 2, 30, '', 2);
+    frames.unshift('frame02');
+
+    stars = game.add.group();
+
+    for (var i = 0; i < 128; i++)
+    {
+        stars.create(game.world.randomX, game.world.randomY, 'star');
     }
-    
-    var bouncy;
-    
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+
+    baddies = game.add.group();
+
+    for (var i = 0; i < 16; i++)
+    {
+        baddies.create(game.world.randomX, game.world.randomY, 'baddie');
     }
-    
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
+
+    lazers = game.add.group();
+
+    player = game.add.sprite(100, 300, 'player');
+    player.anchor.x = 0.5;
+
+    game.camera.follow(player, Phaser.Camera.FOLLOW_LOCKON, 0.1);
+
+    cursors = game.input.keyboard.createCursorKeys();
+    fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    prevCamX = game.camera.x;
+
+}
+
+function update () {
+
+    if (cursors.left.isDown)
+    {
+        player.x -= 8;
+        player.scale.x = -1;
     }
+    else if (cursors.right.isDown)
+    {
+        player.x += 8;
+        player.scale.x = 1;
+    }
+
+    if (cursors.up.isDown)
+    {
+        player.y -= 8;
+    }
+    else if (cursors.down.isDown)
+    {
+        player.y += 8;
+    }
+
+    if (fireButton.isDown)
+    {
+        fireBullet();
+    }
+
+    lazers.forEachAlive(updateBullets, this);
+
+    prevCamX = game.camera.x;
+
+}
+
+function updateBullets (lazer) {
+
+    // if (game.time.now > frameTime)
+    // {
+    //     frameTime = game.time.now + 500;
+    // }
+    // else
+    // {
+    //     return;
+    // }
+
+    //  Adjust for camera scrolling
+    var camDelta = game.camera.x - prevCamX;
+    lazer.x += camDelta;
+
+    if (lazer.animations.frameName !== 'frame30')
+    {
+        lazer.animations.next();
+    }
+    else
+    {
+        if (lazer.scale.x === 1)
+        {
+            lazer.x += 16;
+
+            if (lazer.x > (game.camera.view.right - 224))
+            {
+                lazer.kill();
+            }
+        }
+        else
+        {
+            lazer.x -= 16;
+
+            if (lazer.x < (game.camera.view.left - 224))
+            {
+                lazer.kill();
+            }
+        }
+    }
+
+}
+
+function fireBullet () {
+
+    if (game.time.now > bulletTime)
+    {
+        //  Grab the first bullet we can from the pool
+        lazer = lazers.getFirstDead(true, player.x + 24 * player.scale.x, player.y + 8, 'lazer');
+
+        lazer.animations.add('fire', frames, 60);
+        lazer.animations.frameName = 'frame02';
+
+        lazer.scale.x = player.scale.x;
+
+        if (lazer.scale.x === 1)
+        {
+            // lazer.anchor.x = 1;
+        }
+        else
+        {
+            // lazer.anchor.x = 0;
+        }
+
+        //  Lazers start out with a width of 96 and expand over time
+        // lazer.crop(new Phaser.Rectangle(244-96, 0, 96, 2), true);
+
+        bulletTime = game.time.now + 250;
+    }
+
 };
