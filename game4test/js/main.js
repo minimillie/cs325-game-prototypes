3f7b0b243454b937feab43be2d8280c4c3ecbf4f
minimillie <60392831+minimillie@users.noreply.github.com>
2020-03-05 23:03:47 -0500

    Update main.js

--- game4test/js/main.js
+++ game4test/js/main.js
@@ -280,13 +280,14 @@ function createUI() {
     saveIcon.input.useHandCursor = true;
     saveIcon.events.onInputDown.add(save, this);
     
-    music = game.add.audio('utopia');
-    music = game.add.audio
-    music.play();
-    
     chicken1 = game.add.sprite(100, 300, 'minecraft');
 
 }
+    
+    function create() {
+          music = game.add.audio('utopia');
+          music.play(); 
+    }
 
 function createDrawingArea() {
 
