b900048b661985cc2d86fc06422bfb02d98585c7
minimillie <60392831+minimillie@users.noreply.github.com>
2020-04-09 01:03:50 -0400

    Update main.js

--- game5final/js/main.js
+++ game5final/js/main.js
@@ -96,13 +96,13 @@ function create() {
     player = game.add.sprite(64, 100, 'dude');
     game.physics.arcade.enable(player);
     game.physics.arcade.enable(door);
-    game.physic.arcade.enable(t);
+    //game.physic.arcade.enable(t);
     game.physics.arcade.gravity.y = 350;
 
     player.body.bounce.y = 0.1;
     player.body.collideWorldBounds = true;
     door.body.collideWorldBounds = true;
-    t.body.collideWorldBounds = true;
+    //t.body.collideWorldBounds = true;
     //ground.body.immovable = true;
     player.body.setSize(20, 32, 5, 16);
 
